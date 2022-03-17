8a3021b1adc7a842b6527edf58aabc5cbb6654c2
vishaljagtap3 <vishal.jagtap@bitcode.in>
2022-03-17 14:56:23 +0530

    Different ways to add listeners Adding and removing views dynamically

--- app/src/main/java/in/bitcode/firstapp/MainActivity.java
+++ app/src/main/java/in/bitcode/firstapp/MainActivity.java
@@ -12,33 +12,135 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private LinearLayout linearLayout;
-    private TextView txtTitle;
+    private TextView txtTitle, txtClear;
     private EditText edtUsername, edtPassword;
-    private Button btnSignIn;
+    private Button btnSignIn, btnAddImage, btnRemoveImage;
+
+    ArrayList<ImageView> imageViews = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        linearLayout = findViewById(R.id.container);
         txtTitle = findViewById(R.id.txtTitle);
         edtUsername = findViewById(R.id.edtUsername);
         edtPassword = findViewById(R.id.edtPassword);
         btnSignIn = findViewById(R.id.btnSignIn);
+        txtClear = findViewById(R.id.txtClear);
+        btnAddImage = findViewById(R.id.btnAddImage);
+        btnRemoveImage = findViewById(R.id.btnRemoveImage);
 
-        btnSignIn.setOnClickListener(new BtnSignInClickListener());
+        btnAddImage.setOnClickListener(new BtnAddImageClickListener());
+
+        btnRemoveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(imageViews.size() == 0) {
+                    return;
+                }
+
+                linearLayout.removeView(
+                        imageViews.get( imageViews.size() -1 )
+                );
+                imageViews.remove(imageViews.size()-1);
+            }
+        });
+
+        View.OnClickListener onClickListener = new MyViewClickListener();
+
+        txtClear.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
+
+        /*txtClear.setOnClickListener(onClickListener);
+        btnSignIn.setOnClickListener(onClickListener);*/
+
+        /*btnSignIn.setOnClickListener(new BtnSignInClickListener());
+        txtClear.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        edtUsername.setText("");
+                        edtPassword.setText("");
+                    }
+                }
+        );*/
 
         //btnSignIn.setOnTouchListener(new BtnSignInTouchListener());
         //btnSignIn.setOnKeyListener(new BtnSignKeyListener());
 
         //initUi();
     }
+
+    private class BtnAddImageClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            ImageView img = new ImageView(MainActivity.this);
+            img.setLayoutParams(
+                    new ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.WRAP_CONTENT,
+                            ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+            );
+            img.setImageResource(R.drawable.app_icon);
+
+            linearLayout.addView(img);
+
+            imageViews.add(img);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == txtClear) {
+            edtUsername.setText("");
+            edtPassword.setText("");
+        }
+
+        if(view.getId() == R.id.btnSignIn) {
+            if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode")) {
+                txtTitle.setText("Success");
+            }
+            else {
+                txtTitle.setText("Failed");
+            }
+        }
+    }
+
+    private class MyViewClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            Toast.makeText(MainActivity.this, "view clicked", Toast.LENGTH_LONG)
+                    .show();
+            if(view == txtClear) {
+                edtUsername.setText("");
+                edtPassword.setText("");
+            }
+
+            if(view.getId() == R.id.btnSignIn) {
+                if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode")) {
+                    txtTitle.setText("Success");
+                }
+                else {
+                    txtTitle.setText("Failed");
+                }
+            }
+        }
+    }
+
+
 
     private class BtnSignInClickListener implements View.OnClickListener {
         @Override
